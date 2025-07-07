035a5834adf0986f79f31748db4497a056ad6d69
Jaspreet-Singh5 <jaswindersingh705@gmail.com>
2025-07-08 00:19:30 +0530

    init: Add SearchPanel component with basic structure and integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchPanelComponent } from './search-panel/search-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchPanelComponent
   ],
   imports: [
     BrowserModule,
