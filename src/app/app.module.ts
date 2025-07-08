1423076adac8136a8314be51e29eac4ff9ee1edc
Jaspreet-Singh5 <jaswindersingh705@gmail.com>
2025-07-08 13:21:56 +0530

    refactor: move DataTable and SearchPanel components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SearchPanelComponent } from './search-panel/search-panel.component';
+import { SearchPanelComponent } from './components/search-panel/search-panel.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
@@ -11,7 +11,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { ReactiveFormsModule } from '@angular/forms';
-import { DataTableComponent } from './data-table/data-table.component';
+import { DataTableComponent } from './components/data-table/data-table.component';
 import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
